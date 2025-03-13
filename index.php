823855d938f64ef1cc940eec5aeb13be5efec97d
VJBSilva <valdecyjunior@hotmail.com>
2025-03-13 20:15:44 -0300

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
             font-family: Arial, sans-serif;
             margin: 0;
             padding: 0;
-            background-image: url('imagens/boby.jpg');
+            background-image: url('clinicamaisviver/imagens/boby.jpg');
             background-size: cover;
             background-position: center;
             background-repeat: no-repeat;
