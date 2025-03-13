1350975347c5843f4d8ab9d1b3ffdfa55feab475
VJBSilva <valdecyjunior@hotmail.com>
2025-03-13 20:50:57 -0300

    Update GerenciarArquivos.php

--- admin/GerenciarArquivos.php
+++ admin/GerenciarArquivos.php
@@ -21,7 +21,7 @@ if (!isset($_SESSION['usuario'])) {
             font-family: Arial, sans-serif;
             margin: 0;
             padding: 0;
-            background-image: url('imagens/boby.png'); /* Caminho da imagem atualizado*/
+            background-image: url('../imagens/boby.png'); /* Caminho da imagem atualizado*/
             background-size: cover;
             background-position: center;
             background-repeat: no-repeat;
